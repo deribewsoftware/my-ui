1d40d5295aad8df480801811a9f830f2c9197258
deribewsoftware <deribewsoftware@gmail.com>
2024-03-04 09:59:48 +0300

    updated

--- components/textEditor.tsx
+++ components/textEditor.tsx
@@ -5,8 +5,11 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
  const TextEditor = () => {
   const [value, setValue] = useState('');
+  const valueData="<p>my name is <h1>Deribew</h1> Shimelis I live in Addis Ababa </p>"
 
-  return <ReactQuill className='border rounded-lg focus:border-blue-600' theme="bubble" value={value} onChange={setValue} placeholder='Write Here the Contents ' />;
+  return <><ReactQuill  className='border text-lg rounded-lg focus:border-blue-600' theme="bubble" value={value? value:valueData}   onChange={setValue} placeholder='Write Here the Contents ' />
+  <div className="">{value}</div>
+  </>;
  }
   
  export default TextEditor;
