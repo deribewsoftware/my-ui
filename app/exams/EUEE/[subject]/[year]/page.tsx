e3e97dd17fee040b2883baf1b40e9bd0250e1936
deribewsoftware <deribewsoftware@gmail.com>
2024-03-08 16:00:41 +0300

    updated

--- app/exams/EUEE/[subject]/[year]/page.tsx
+++ app/exams/EUEE/[subject]/[year]/page.tsx
@@ -1,145 +1,16 @@
-"use client"
-import SubLayout from '@/components/layouts/SubLayout'
-import MainLayout from '@/components/layouts/mainLayout'
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
-import { Questions } from '@/lib/questions'
-import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from '@radix-ui/react-accordion'
-import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import { Suspense } from "react"
+import EUEEYearClientPage from "./yearClientPage"
 
 const EUEEExamYearPage = () => {
 
-  const router=useRouter();
-  const [feedbackVisible, setFeedbackVisible] = useState(false);
-  const [isSelectedAll,setSelectedAll]=useState(false);
-  const [score, setScore] = useState(0);
-  const [showQuiz,setShowQuiz] = useState(false);
-const [selectedChoices, setSelectedChoices] = useState<any>({});
+  
+return(<div><Suspense >
+    <EUEEYearClientPage/>
+    </Suspense>
 
-const onSelectionChanged = (questionId:number, choiceIndex:string, isAnswer:boolean) => {
-  const updatedChoices = {
-    ...selectedChoices,
-    [questionId]: {
-      choiceIndex,
-      isCorrect: isAnswer,
-    },
-  };
-  setSelectedChoices(updatedChoices);
+</div>)
+
  
-  
-  
-   // Calculate score
-   const newScore = Object.values(updatedChoices).reduce(
-    (acc:number, choice:any) => (choice.isCorrect ? acc + 1 : acc),
-    0
-  );
-  setScore(newScore);
-};
-
-const onSubmit=()=>{
-  setFeedbackVisible(true);
-  setSelectedAll(true);
-}
-
-  return (
-  <MainLayout>
-    <SubLayout>
-    <div className="pt-10" id="quiz">
-    <div className="p-2 py-10">
-      <h4 className="text-lg font-bold border-b-2 border-dashed">Quizzes of Indroduction of Biology</h4>
-    </div>
-      {
-        Questions.map((question:any,index)=>{
-        return <div key={index} className="">
-
-<div className="flex border-b border-double border-green-600 justify-between">
-        <p className="text-xl font-bold">{index+1}</p>
-        <p className="text-gray-500 text-sm">{question.year}</p>
-      </div>
-      <p className="p-2">{question.title}</p>
-      <div className="p-2 space-y-2">
-        {
-          question.chooses.map((choice:any,ind:number) =>{
-            return <div key={ind}   
-            className={`flex gap-2 p-2 ${
-              (feedbackVisible&&selectedChoices[index+1]&& choice.isAnswer)&&'bg-green-200'||isSelectedAll&& choice.isAnswer&&'bg-green-200'
-            } ${
-              selectedChoices[(index+1)]?.choiceIndex === ind.toString() &&feedbackVisible&&
-              selectedChoices[(index+1)]?.isCorrect
-                ? 'bg-green-200'
-                : feedbackVisible&&selectedChoices[(index+1)]?.choiceIndex === ind.toString()
-                ? 'bg-red-200'
-                : ''
-            }`}>
-             <button
-             disabled={(selectedChoices[index+1]&&feedbackVisible)||isSelectedAll}
-    onClick={() => {
-      onSelectionChanged(
-       index+1,
-        ind.toString(),
-        choice.isAnswer
-      );
-    }}
-    className={`h-4 w-4 disabled:cursor-not-allowed disabled:outline-blue-400  outline outline-2 border-2 border-white outline-blue-500 rounded-full  ${
-      selectedChoices[index+1]?.choiceIndex === ind.toString()
-        ? 'bg-blue-400 disabled:bg-blue-300'
-        : 'bg-white'
-    }`}
-  >
-    
-  </button>
-            <p>{choice.text}</p>
-          </div>
-          })
-        }
-       
-      </div>
-      {
-  (selectedChoices[index+1] && feedbackVisible) || isSelectedAll ? (
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1" className="border-none">
-        <AccordionTrigger className="hover:no-underline">Explanation</AccordionTrigger>
-        <AccordionContent>
-          <div className="p-2 bg-green-50">
-            <p>{question.explanation}</p>
-          </div>
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-  ) : (
-    ''
-  )
-}
-
-
-
-
-        </div>
-        })
-      }
-
-
-      <div className="flex justify-end p-4 ">
-     
-     <Dialog>
-      <DialogTrigger> <button 
-      onClick={onSubmit}
-       className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit
-       </button></DialogTrigger>
-       <DialogContent>
-        <div className="flex justify-center w-full">
-        <div className="w-48 h-48 flex justify-center items-center rounded-full border-2">
-          <p className="text-2xl font-bold">{score}/{Questions.length}</p>
-        </div>
-        </div>
-       </DialogContent>
-     </Dialog>
-
-
-      </div></div>
-    </SubLayout>
-  </MainLayout>
-  )
 }
 
 export default EUEEExamYearPage
--- components/card/yearExamCard.tsx
+++ components/card/yearExamCard.tsx
@@ -2,16 +2,20 @@
 import React from 'react'
 import StartExamDialogue from '../startExamDialog'
 import { useRouter } from 'next/navigation'
-interface YearExamCardProps{
 
-}
 const YearExamCard = () => {
   const router=useRouter();
+  const onCheckRight=()=>{
+    const encodedQuery=encodeURIComponent(true);
+    router.push(`/exams/EUEE/Biology/2016?right=${encodedQuery}`)
+  }
   return (
     <div  className="no-underline block w-full md:max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
      <div className="flex flex-col gap-4 justify-center items-center p-2">
       <h1 className='text-lg font-semibold'>2016 E.C  Year Exams</h1>
-      <StartExamDialogue onCheckRightAnswer={()=>{router.push('/exams/EUEE/Biology/2016')}} onShowAnswerAfterQuiz={()=>{router.push('/exams/EUEE/Biology/2016')}}/>
+      <StartExamDialogue 
+      onCheckRightAnswer={onCheckRight} 
+      onShowAnswerAfterQuiz={()=>{router.push('/exams/EUEE/Biology/2016')}}/>
      </div>
     </div>
   )
