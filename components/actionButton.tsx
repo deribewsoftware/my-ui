3918694d636a7757313a5f6cdac3f2b98c2b80ba
deribewsoftware <deribewsoftware@gmail.com>
2024-03-23 12:00:59 +0300

    updated

--- components/actionButton.tsx
+++ components/actionButton.tsx
@@ -9,13 +9,13 @@ interface ActionButtonProps{
   url:string;
 }
 const ActionButton:React.FC<ActionButtonProps> = ({label,url,icon:Icon}) => {
-  return (<Link  href={url} className="py-2.5 px-5 me-2 mb-2
+  return (<Link  href={url} className="py-2 md:py-2.5 px-3 md:px-5 me-2 mb-2
   text-sm font-medium text-gray-900 focus:outline-none
    bg-white rounded-full border border-gray-200 
    hover:bg-gray-100 hover:text-blue-700 focus:z-10 
    focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700
     dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600
-     dark:hover:text-white dark:hover:bg-gray-700 flex gap-2 items-center"><Icon size={24}/> <p>{label}</p></Link>
+     dark:hover:text-white dark:hover:bg-gray-700 flex gap-2 items-center justify-center"><Icon size={24}/> <p>{label}</p></Link>
   )
 }
 
--- components/chapterList.tsx
+++ components/chapterList.tsx
@@ -1,4 +1,21 @@
 
+"use client"
+
+import * as React from "react"
+import { GrFormView } from "react-icons/gr";
+import { AiOutlineDelete } from "react-icons/ai"
+import { Button } from "@/components/ui/button"
+import { MdEditNote } from "react-icons/md";
+import { MdEditDocument } from "react-icons/md";
+import { BsThreeDots } from "react-icons/bs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import {
   Accordion,
   AccordionContent,
@@ -9,20 +26,22 @@ import { chapterList } from "@/lib/chapterList"
 import { IoMdAdd } from "react-icons/io";
 import { MdModeEdit } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
+import { GoQuestion } from "react-icons/go";
 import ActionButton from "./actionButton";
+import Link from "next/link"
 export function ChapterList() {
   return (
     <Accordion type="single" collapsible className="w-full">
       {chapterList.map((chapter,index)=>{
         return <AccordionItem key={index} value={`${index}`}>
         <AccordionTrigger className="hover:no-underline">
-        <h5 className="flex text-[14px] gap-2 font-medium"><p>Chapter {index+1}: </p><p>{chapter.title}</p></h5>
+        <h5 className="flex text-[14px] gap-2 font-medium px-2 md:px-6"><p>Chapter {index+1}: </p><p>{chapter.title}</p></h5>
         </AccordionTrigger>
         <AccordionContent>
         <div className="flex flex-col">
 
-          <div className="p-4 flex justify-between">
-            <div className="flex gap-4">
+          <div className="p-4 flex flex-col md:flex-row md:justify-between">
+            <div className="flex flex-col md:flex-row  md:gap-4">
             <ActionButton 
             label="Edit Chapter"
             url="/"
@@ -38,9 +57,9 @@ export function ChapterList() {
             icon={IoMdAdd}/>
           </div>
   <div className="-m-1.5 overflow-x-auto">
-    <div className="p-1.5 min-w-full inline-block align-middle">
+    <div className="p-1.5 min-w-full inline-block align-middle relative">
       <div className="overflow-hidden">
-        <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+        <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700 relative">
           <thead>
             <tr>
               <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Lessons</th>
@@ -52,10 +71,41 @@ export function ChapterList() {
            {
             chapter.lessons.map((lesson,ind)=>{
               return  <tr key={ind} className="hover:bg-gray-100 dark:hover:bg-gray-700">
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">Lesson: {ind+1}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{lesson.title}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-                <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Delete</button>
+              <td className="px-6 py-2 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">Lesson: {ind+1}</td>
+              <td className="px-6 py-2 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{lesson.title}</td>
+              <td className="py-2 whitespace-nowrap text-end text-sm font-medium relative">
+             
+             <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost"><BsThreeDots size={20}/></Button>
+      </DropdownMenuTrigger>
+      
+      <DropdownMenuContent className="w-56 absolute right-0 top-2">
+        <DropdownMenuLabel>Lesson: {ind+1}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem >
+          <Link href={`/course`} className="flex no-underline gap-2 items-center text-sm"><GrFormView size={20}/> <p>View Detail</p></Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem >
+          <Link href={`/course`} className="flex no-underline gap-2 items-center text-sm"><GoQuestion size={20}/> <p>Lesson Questions</p></Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+        <Link href={`/course`} className="flex no-underline gap-2 items-center text-sm"><MdEditNote size={20}/> <p>Edit Lesson</p></Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+        <Link href={`/course`} className="flex no-underline gap-2 items-center text-sm"><MdEditDocument size={20}/> <p>Edit Lesson Files</p></Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem>
+        <Link href={`/course`} className="flex no-underline gap-2 items-center text-sm"><AiOutlineDelete size={20}/> <p>Delete Lesson</p></Link>
+
+        </DropdownMenuItem>
+        
+       
+      </DropdownMenuContent>
+    
+    </DropdownMenu>
+            
               </td>
             </tr>
             })
@@ -75,3 +125,6 @@ export function ChapterList() {
     </Accordion>
   )
 }
+
+
+
